c034e80773c63f48dd052a3971857aac13f99080
ikrama sunasra <ikrama.s@somaiya.edu>
2024-02-27 07:56:22 +0530

    1st commit

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -70,110 +70,110 @@ class _HomepageState extends State<Homepage> {
               ))
         ],
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hi ${userName} 👋",
-                    style: TextStyle(
-                        color: kLightGreen,
-                        fontSize: 32,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Text("Expore event around you",
-                      style: TextStyle(
-                          color: kLightGreen,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600)),
-                  isLoading
-                      ? const SizedBox()
-                      : CarouselSlider(
-                          options: CarouselOptions(
-                            autoPlay: true,
-                            autoPlayInterval: const Duration(seconds: 5),
-                            aspectRatio: 16 / 9,
-                            viewportFraction: 0.99,
-                            enlargeCenterPage: true,
-                            scrollDirection: Axis.horizontal,
-                          ),
-                          items: [
-                            ...List.generate(4, (index) {
-                              return EventContainer(
-                                data: events[index],
-                              );
-                            }),
-                          ],
-                        ),
-                  const SizedBox(height: 16),
-                  Text(
-                    "Popular Events ",
-                    style: TextStyle(
-                      color: kLightGreen,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              Container(
-                color: const Color(0xFF2E2E2E),
-                child: isLoading
-                    ? const SizedBox()
-                    : Column(
-                        children: [
-                          for (int i = 0; i < events.length && i < 5; i++) ...[
-                            PopularItem(
-                              eventData: events[i],
-                              index: i + 1,
-                            ),
-                            const Divider(),
-                          ],
-                        ],
-                      ),
-              )
-            ]),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding:
-                  const EdgeInsets.only(bottom: 2, top: 8, left: 6, right: 6),
-              child: Text(
-                "All Events",
-                style: TextStyle(
-                  color: kLightGreen,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          SliverList(
-              delegate: SliverChildBuilderDelegate(
-                  (context, index) => EventContainer(data: events[index]),
-                  childCount: events.length)),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await Navigator.push(context,
-              MaterialPageRoute(builder: (context) => CreateEventPage()));
-          refresh();
-        },
-        child: Icon(
-          Icons.add,
-          color: Colors.black,
-        ),
-        backgroundColor: kLightGreen,
-      ),
+      // body: CustomScrollView(
+      //   slivers: [
+      //     SliverToBoxAdapter(
+      //       child: Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: Column(
+      //           crossAxisAlignment: CrossAxisAlignment.start,
+      //           children: [
+      //             Text(
+      //               "Hi ${userName} 👋",
+      //               style: TextStyle(
+      //                   color: kLightGreen,
+      //                   fontSize: 32,
+      //                   fontWeight: FontWeight.w600),
+      //             ),
+      //             Text("Expore event around you",
+      //                 style: TextStyle(
+      //                     color: kLightGreen,
+      //                     fontSize: 16,
+      //                     fontWeight: FontWeight.w600)),
+      //             isLoading
+      //                 ? const SizedBox()
+      //                 : CarouselSlider(
+      //                     options: CarouselOptions(
+      //                       autoPlay: true,
+      //                       autoPlayInterval: const Duration(seconds: 5),
+      //                       aspectRatio: 16 / 9,
+      //                       viewportFraction: 0.99,
+      //                       enlargeCenterPage: true,
+      //                       scrollDirection: Axis.horizontal,
+      //                     ),
+      //                     items: [
+      //                       ...List.generate(4, (index) {
+      //                         return EventContainer(
+      //                           data: events[index],
+      //                         );
+      //                       }),
+      //                     ],
+      //                   ),
+      //             const SizedBox(height: 16),
+      //             Text(
+      //               "Popular Events ",
+      //               style: TextStyle(
+      //                 color: kLightGreen,
+      //                 fontSize: 24,
+      //                 fontWeight: FontWeight.bold,
+      //               ),
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+      //     SliverList(
+      //       delegate: SliverChildListDelegate([
+      //         Container(
+      //           color: const Color(0xFF2E2E2E),
+      //           child: isLoading
+      //               ? const SizedBox()
+      //               : Column(
+      //                   children: [
+      //                     for (int i = 0; i < events.length && i < 5; i++) ...[
+      //                       PopularItem(
+      //                         eventData: events[i],
+      //                         index: i + 1,
+      //                       ),
+      //                       const Divider(),
+      //                     ],
+      //                   ],
+      //                 ),
+      //         )
+      //       ]),
+      //     ),
+      //     SliverToBoxAdapter(
+      //       child: Padding(
+      //         padding:
+      //             const EdgeInsets.only(bottom: 2, top: 8, left: 6, right: 6),
+      //         child: Text(
+      //           "All Events",
+      //           style: TextStyle(
+      //             color: kLightGreen,
+      //             fontSize: 24,
+      //             fontWeight: FontWeight.bold,
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //     SliverList(
+      //         delegate: SliverChildBuilderDelegate(
+      //             (context, index) => EventContainer(data: events[index]),
+      //             childCount: events.length)),
+      //   ],
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () async {
+      //     await Navigator.push(context,
+      //         MaterialPageRoute(builder: (context) => CreateEventPage()));
+      //     refresh();
+      //   },
+      // child: Icon(
+      //   Icons.add,
+      //   color: Colors.black,
+      // ),
+      //   backgroundColor: kLightGreen,
+      // ),
     );
   }
 }
